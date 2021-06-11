4b9923c10ce048af602484321c2979a37af7c773
张义 <sht@zhangyideMacBook-Pro-company.local>
2021-06-11 14:40:53 +0800

    feat: component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,39 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2021 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+import { GlobalDataProps } from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
-import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-
-const currentUser: UserProps = {
-  isLogin: true,
-  name: '于谦',
-  id: 12
-}
-
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的标题',
-    description: 'test1的描述：理科生居第二位，深刻的垃圾分类',
-    avatar: 'https://img0.baidu.com/it/u=1531038383,3455172191&fm=26&fmt=auto&gp=0.jpg'
-  },
-  {
-    id: 2,
-    title: 'test2的标题',
-    description: 'test2的描述：理科生居第二位，死了都快使点劲',
-    avatar: 'https://img2.baidu.com/it/u=2306696130,3636777462&fm=26&fmt=auto&gp=0.jpg'
-  },
-  {
-    id: 3,
-    title: 'test2的标题',
-    description: 'test2的描述：理科生居第二位，死了都快使点劲'
-  },
-  {
-    id: 4,
-    title: 'test2的标题',
-    description: 'test2的描述：理科生居第二位，死了都快使点劲',
-    avatar: 'https://img2.baidu.com/it/u=2306696130,3636777462&fm=26&fmt=auto&gp=0.jpg'
-  }
-]
+import GlobalHeader from './components/GlobalHeader.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
     GlobalHeader
   },
   setup () {
+    const store = useStore<GlobalDataProps>()
+    const currentUser = computed(() => {
+      return store.state.user
+    })
     return {
-      list: testData,
       currentUser
     }
   }
@@ -64,6 +46,5 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail
+    }
+  ]
+})
+
+export default router
